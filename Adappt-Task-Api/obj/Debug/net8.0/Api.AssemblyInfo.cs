3f53c58521520241a5c1ee1600b807db7a50ad03
Shahd Faris <shahdfaris.e@adappt.ai>
2024-05-09 17:49:36 +0300

    fix conflicts in refrencing the another project

--- Adappt-Task-Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Adappt-Task-Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b9b328dca0784178ba3818ad0604036d7b9896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8972c21e148ca0633cdc74b2c1004ced55b23ffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
